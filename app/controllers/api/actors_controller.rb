592463f0a08b77aa7edbe83206298d74f7b9afd3
Kelly <kellybucheger@gmail.com>
2019-05-13 09:39:25 -0400

    adds today's homework

--- app/controllers/api/actors_controller.rb
+++ app/controllers/api/actors_controller.rb
@@ -4,4 +4,19 @@ class Api::ActorsController < ApplicationController
     @actor = Actor.find_by(id: 6)
     render "single_actor.json.jbuilder"
   end
+
+  def actor_id
+
+    # id  = params[:id]
+    # @actor = Actor.find_by("id": id)
+
+    @actor = Actor.find_by("id": params[:id])
+
+    render "actor_by_id.json.jbuilder"
+  end
+
 end
+
+
+   # id = params[:id]
+   #  @movie = Movie.find_by("id": id)
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   namespace :api do
     get "/single_actor_url" => "actors#single_actor_action"
 
+    get "actor" => "actors#actor_id"
+
+    get "actor/:id" => "actors#actor_id"
+
+    post "actor" => "actors#actor_id"
+
     get "/all_movies_url" => "movies#display_all"
 
     get "/movie_by_id/:id" => "movies#movie_by_id"
