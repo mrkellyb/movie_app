56816385b26651fa830e9b084f6ca9ce61bfc310
Kelly <kellybucheger@gmail.com>
2019-05-24 09:42:42 -0400

    Adds today's homework

--- config/routes.rb
+++ config/routes.rb
@@ -3,26 +3,20 @@ Rails.application.routes.draw do
   namespace :api do
 
     get "/movies" => "movies#index"
-
     get "/movies/:id" => "movies#show"
-
-
     post "/movies" => "movies#create"
-
     patch "/movies/:id" => "movies#update"
-
     delete "/movies/:id" => "movies#destroy"
 
     get "/actors" => "actors#index"
-
     get "/actors/:id" => "actors#show"
-
-
     post "/actors" => "actors#create"
-
     patch "/actors/:id" => "actors#update"
-
     delete "/actors/:id" => "actors#destroy"
+
+    post "/users" => "users#create"
+
+    post "/sessions" => "sessions#create"
 
 
   end
