145f594ae85c95cddecfcd53248a5911d4682be6
Kelly <kellybucheger@gmail.com>
2019-06-12 19:51:56 -0400

    adds reordered seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,3 @@
-Actor.create!([
-  {first_name: "Robin", last_name: "Williams", known_for: "Mrs. Doubtfire", gender: "M", age: 63, movie_id: 19},
-  {first_name: "Patrick", last_name: "Stewart", known_for: "Dune", gender: "M", age: 78, movie_id: 17},
-  {first_name: "Emma", last_name: "Stone", known_for: "Crazy,  Stupid, Love", gender: "F", age: 30, movie_id: 20},
-  {first_name: "Dustin", last_name: "Hoffman", known_for: "The Graduate", gender: "M", age: 81, movie_id: 18},
-  {first_name: "Meryl", last_name: "Streep", known_for: "Sophie's Choice", gender: "F", age: 69, movie_id: 16},
-  {first_name: "Harrison", last_name: "Ford", known_for: "Blade Runner", gender: "M", age: 76, movie_id: 2},
-  {first_name: "Benedict", last_name: "Cumberbatch", known_for: "Dune", gender: "M", age: 42, movie_id: 15}
-])
-Genre.create!([
-  {name: "thriller"},
-  {name: "romcom"},
-  {name: "action"},
-  {name: "scifi"},
-  {name: "comedy"},
-  {name: "drama"},
-  {name: "musical"},
-  {name: "foreign"},
-  {name: "fantasy"}
-])
 Movie.create!([
   {title: "Being John Malkovitch", year: 1999, plot: "Shit gets weird.", director: "Spike Jonze", english: true},
   {title: "Diva", year: 1982, plot: "Enjoy this Paris chase scene!", director: "Jean-Jacques Beinix", english: false},
@@ -39,6 +19,36 @@ Movie.create!([
   {title: "The Sting", year: 1973, plot: "Enjoy this Scott Joplin music!", director: "George Roy Hill", english: true},
   {title: "Birdman", year: 2014, plot: "Shit's about to get weird, possible actual superhero.", director: "Alejandro G. Inarritu", english: true}
 ])
+
+Actor.create!([
+  {first_name: "Robin", last_name: "Williams", known_for: "Mrs. Doubtfire", gender: "M", age: 63, movie_id: 19},
+  {first_name: "Patrick", last_name: "Stewart", known_for: "Dune", gender: "M", age: 78, movie_id: 17},
+  {first_name: "Emma", last_name: "Stone", known_for: "Crazy,  Stupid, Love", gender: "F", age: 30, movie_id: 20},
+  {first_name: "Dustin", last_name: "Hoffman", known_for: "The Graduate", gender: "M", age: 81, movie_id: 18},
+  {first_name: "Meryl", last_name: "Streep", known_for: "Sophie's Choice", gender: "F", age: 69, movie_id: 16},
+  {first_name: "Harrison", last_name: "Ford", known_for: "Blade Runner", gender: "M", age: 76, movie_id: 2},
+  {first_name: "Benedict", last_name: "Cumberbatch", known_for: "Dune", gender: "M", age: 42, movie_id: 15}
+])
+
+User.create!([
+  {name: "goodkelly", email: "goodkelly@email.com", password_digest: "$2a$10$Hlzzp5tExOwabJDIdyb7AOJ8KCI91l/XeoIZDDDa9FtPrz7QBIBMC", admin: false},
+  {name: "murley", email: "murley@email.com", password_digest: "$2a$10$51JHW6RVXUKMtOBPE5Jx8Oomzb2JHX7BqaJeEgomxNyFEWqZ6H9a.", admin: false},
+  {name: "badkelly", email: "badkelly@email.com", password_digest: "$2a$10$PP9eCLVtNN0oH8u4anJKEO258ukcJcXVPW.9Ewb4pjvDqjB5BTLDG", admin: false},
+  {name: "kelly", email: "kelly@email.com", password_digest: "$2a$10$u.IT20VZjcc3b.ujLDnSteEYJ2LUvIP2zNpfpncqBRZpJCpPHnyeS", admin: true}
+])
+
+Genre.create!([
+  {name: "thriller"},
+  {name: "romcom"},
+  {name: "action"},
+  {name: "scifi"},
+  {name: "comedy"},
+  {name: "drama"},
+  {name: "musical"},
+  {name: "foreign"},
+  {name: "fantasy"}
+])
+
 MovieGenre.create!([
   {genre_id: 4, movie_id: 1},
   {genre_id: 4, movie_id: 2},
@@ -51,9 +61,4 @@ MovieGenre.create!([
   {genre_id: 7, movie_id: 10},
   {genre_id: 9, movie_id: 12}
 ])
-User.create!([
-  {name: "goodkelly", email: "goodkelly@email.com", password_digest: "$2a$10$Hlzzp5tExOwabJDIdyb7AOJ8KCI91l/XeoIZDDDa9FtPrz7QBIBMC", admin: false},
-  {name: "murley", email: "murley@email.com", password_digest: "$2a$10$51JHW6RVXUKMtOBPE5Jx8Oomzb2JHX7BqaJeEgomxNyFEWqZ6H9a.", admin: false},
-  {name: "badkelly", email: "badkelly@email.com", password_digest: "$2a$10$PP9eCLVtNN0oH8u4anJKEO258ukcJcXVPW.9Ewb4pjvDqjB5BTLDG", admin: false},
-  {name: "kelly", email: "kelly@email.com", password_digest: "$2a$10$u.IT20VZjcc3b.ujLDnSteEYJ2LUvIP2zNpfpncqBRZpJCpPHnyeS", admin: true}
-])
+
